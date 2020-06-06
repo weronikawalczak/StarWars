3ce9366d7cb578db1afd5620c651f2ddef9f5435
weronikawalczak989 <weronikawalczak989@gmail.com>
2020-06-06 20:27:45 +0200

    Initial commit

---
+++ src/test/java/com/weronika/nask/NaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weronika.nask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
